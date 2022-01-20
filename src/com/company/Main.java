c6213da0ce0bc817d938407dd171d8db1904da22
Rafeeq Muhammad <rafeeqnmuhammad@gmail.com>
2022-01-19 22:31:55 -0600

    fixed problem 1, made with an actual recursive function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,26 +6,18 @@ package com.company;
 public class Main {
 
     //Problem 1
-    static int p1_global_total = 0;
-
     public static void main(String[] args) {
-	// write your code here
-	System.out.println("Hello world!");
-	System.out.println(recursiveAddition(10));
+	System.out.println(recursiveAddition(4));
     }
 
     //Problem 1
     public static int recursiveAddition(int n) {
-        //int local_total = 0;
-
-        if (n == 1) {
-            return 1;
+        if (n == 0) {
+            return 0;
         } else {
-            p1_global_total = p1_global_total + n;
-            n = n - 1;
-            recursiveAddition(n);
+            return n+recursiveAddition(n-1);
         }
-        return p1_global_total;
+        //return n+recursiveAddition(n-1);
     }
 
 }
