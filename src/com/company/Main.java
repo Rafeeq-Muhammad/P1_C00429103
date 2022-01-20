a6a0c7ebbfadf4b3775bff8f792ab339ba3bfc80
Rafeeq Muhammad <rafeeqnmuhammad@gmail.com>
2022-01-19 22:45:26 -0600

    problem 2 implemented and fully functional

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         //Problem 1
         int p1 = 10;
         System.out.print("Problem 1 result: ");
 	    System.out.println(recursiveAddition(p1));
+
+        //Problem 2
+        int m = 10;
+        int n = 9;
+        System.out.print("Problem 2 result: ");
+        System.out.println(gcd(m,n));
     }
 
     //Problem 1
@@ -21,6 +28,14 @@ public class Main {
         }
     }
 
+    //Problem 2
+    public static int gcd(int m, int n) {
+        if (m%n == 0) {
+            return n;
+        } else {
+            return gcd(n, m%n);
+        }
+    }
 
 
 }
